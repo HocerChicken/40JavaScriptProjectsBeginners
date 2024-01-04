8db3936508d10628bd8e29c73b908a1e0fd89114
Pham Thai Hoc <89366106+HocerChicken@users.noreply.github.com>
2024-01-04 23:52:14 +0700

    Initial Counter Project

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -1,1 +1,9 @@
 const colors = ["red", "green", "blue", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+    let randomNumber = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -1,1 +1,19 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+//#xxxxxx
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+    let RandomNumberHex = getRandomHex();
+    document.body.style.backgroundColor = RandomNumberHex;
+    color.textContent = RandomNumberHex;
+});
+function getRandomHex() {
+    let hexInitial = "#";
+    for (let i = 0; i < 6; i++) {
+        let indexRandomHex = Math.floor(Math.random() * hex.length);
+        hexInitial += hex[indexRandomHex];
+    }
+    return hexInitial;
+}
