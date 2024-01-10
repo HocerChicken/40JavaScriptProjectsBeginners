3da43ec01cc003cf1f2362b30e9a8c73a359d680
Pham Thai Hoc <89366106+HocerChicken@users.noreply.github.com>
2024-01-10 13:44:13 +0700

    complete questions

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,2 +1,29 @@
 //using selectors inside the element
+const questions = document.querySelectorAll(".question");
+questions.forEach((question) => {
+    let btn = question.querySelector(".question-btn");
+    btn.addEventListener("click", () => {
+        questions.forEach((item) => {
+            if (item !== question) {
+                item.classList.remove("show-text");
+            }
+        });
+        question.classList.toggle("show-text");
+    });
+});
 // traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+// btns.forEach((btn) => {
+//     btn.addEventListener("click", () => {
+//         let parent = btn.parentElement.parentElement;
+//         parent.classList.toggle("show-text");
+
+//         btns.forEach((otherBtn) => {
+//             if (otherBtn !== btn) {
+//                 otherBtn.parentElement.parentElement.classList.remove(
+//                     "show-text"
+//                 );
+//             }
+//         });
+//     });
+// });
